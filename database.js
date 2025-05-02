1bd267909f0092a6ecf67452a844330f575a0085
pedro-senaa <pedro.s.correa00@gmail.com>
2025-05-02 18:15:59 -0300

    set database connecting

---
+++ database.js
@@ -0,0 +1,44 @@
+const { Pool } = require('pg');
+
+// Create a new pool instance; down the road change user and set password @ render
+const pool = new Pool({
+    user: 'postgres', 
+    host: 'localhost',
+    database: 'personal_budget',
+    password: 'postgres',
+    port: 5432, // Default PostgreSQL port
+});
+
+// Function to test the connection
+async function testConnection() {
+    try {
+        const client = await pool.connect();
+        console.log('Connected to the database successfully!');
+        client.release(); // Release the client back to the pool
+    } catch (err) {
+        console.error('Error connecting to the database:', err);
+    }
+}
+
+// function to create table (in case it gives error)
+async function createEnvelopesTable() {
+    const query = `
+    CREATE TABLE IF NOT EXISTS Envelopes (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    amount INTEGER NOT NULL CHECK (amount >= 0))`
+
+    try {
+        await pool.query(query)
+        console.log('Envelopes table online');
+    } catch (err) {
+        console.error(`Error creating table : ${err}`);
+    } finally {
+        await pool.end();
+    }
+}
+
+testConnection();
+createEnvelopesTable();
+
+module.exports = pool;
